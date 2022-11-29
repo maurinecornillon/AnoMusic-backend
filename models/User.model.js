51a91d4d2accebdefec3cb4037155500aed6f903
Maurine Cornillon <maurinecornillon@mbp-de-maurine.lan>
2022-11-29 17:46:39 +0100

    setup ok

--- models/User.model.js
+++ models/User.model.js
@@ -1,20 +1,46 @@
 const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
-	{
-		name: {
-			type: String,
-		},
-		email: {
-			type: String,
-			unique: true,
-		},
-		password: String,
-	},
-	{
-		// this second object adds extra properties: `createdAt` and `updatedAt`
-		timestamps: true,
-	}
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      validate: [/[^@]/, "@ not allowed on username"],
+    },
+    firstName: {
+      type: String,
+      max: 50,
+    },
+    lastName: {
+      type: String,
+      max: 50,
+    },
+    age: {
+      type: Number,
+    },
+    description: {
+      type: String,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate: [
+        /(\w+\.?|-?\w+?)+@\w+\.?-?\w+?(\.\w{2,3})+/,
+        "not a valid email address",
+      ],
+    },
+    password: String,
+  },
+
+  {
+    timestamps: true,
+  }
 );
 
 module.exports = model("User", userSchema);
